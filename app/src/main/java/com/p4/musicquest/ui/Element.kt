a47164b06bffc99ee56b5f4899dceb64f7dbe154
Aymeric Wibo <obiwac@gmail.com>
2024-04-14 01:57:23 +0200

    ui: Text UI element

--- app/src/main/java/com/p4/musicquest/ui/Element.kt
+++ app/src/main/java/com/p4/musicquest/ui/Element.kt
@@ -7,7 +7,7 @@ import com.p4.musicquest.UI
 import com.p4.musicquest.UIRefCorner
 import android.opengl.GLES30 as gl
 
-open class Element(private val ui: UI, texPath: String? = null, private val refCorner: UIRefCorner, private var x: Float, private var y: Float, private val width: Float, private val height: Float) {
+open class Element(private val ui: UI, texPath: String? = null, private val refCorner: UIRefCorner, private var x: Float, private var y: Float, protected var width: Float, protected var height: Float) {
 	var targetX = x
 	var targetY = y
 
---
+++ app/src/main/java/com/p4/musicquest/ui/Text.kt
@@ -0,0 +1,19 @@
+package com.p4.musicquest.ui
+
+import com.p4.musicquest.Shader
+import com.p4.musicquest.UI
+import com.p4.musicquest.UIRefCorner
+
+class Text(ui: UI, font: Font, text: String, refCorner: UIRefCorner, x: Float, y: Float, width: Float) : Element(ui, null, refCorner, x, y, width, width) {
+	private val tex = font.render(text)
+
+	override fun draw(shader: Shader, dt: Float) {
+		val ratio = tex.xRes.toFloat() / tex.yRes
+		height = width / ratio
+
+		println("font rendering $width $height ${tex.xRes} ${tex.yRes}")
+
+		setTex(shader, tex)
+		super.draw(shader, dt)
+	}
+}
