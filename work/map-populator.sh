5fe37430839a35ae3939e0cefdfc2ac07f43321b
Aymeric Wibo <obiwac@gmail.com>
2024-04-26 11:14:33 +0200

    work/map-populator: Don't forget ;)

--- work/map-populator.sh
+++ work/map-populator.sh
@@ -3,4 +3,4 @@ set -xe
 
 ~/random/obj-to-ivx/obj-to-ivx models/map.obj ../app/src/main/assets/map.ivx
 CompressonatorCLI -log -fd ETC2_RGBA1 textures/map.png ../app/src/main/assets/textures/map.ktx
-cp textures/mask.png ../app/src/main/assets/textures/mask.png
+cp textures/mask.png ../app/src/main/assets/textures/mask.png # TODO Is this okay to be KTX-compressed?
