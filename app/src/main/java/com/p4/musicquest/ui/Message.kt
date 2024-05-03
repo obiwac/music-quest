e17a0b79423cc79ea1cdb8d4cc6fc7ba6f35d704
Akialess <alessandro.giansante03@gmail.com>
2024-05-03 21:31:06 +0200

    Fix Message popup

--- app/src/main/java/com/p4/musicquest/ui/Message.kt
+++ app/src/main/java/com/p4/musicquest/ui/Message.kt
@@ -16,52 +16,28 @@ class Message(context: Context, val ui: UI) {
 
 
 		for (i in 0..<ui.messageTextList.size) {
-			if (ui.messageTextList[i].text == null) {
-				ui.messageTextList[i].text = Text(ui, font, ui.messageTextList[i].contents, UIRefCorner.TOP_LEFT, .1f, .5f, .8f)
-			}
+			if (i < ui.messageTextList.size) {
+				if (ui.messageTextList[i].text == null) {
+					ui.messageTextList[i].text = Text(ui, font, ui.messageTextList[i].contents, UIRefCorner.TOP_LEFT, .1f, messageY, .8f)
+				}
 
-			ui.messageTextList[i].text?.x = messageX
-			ui.messageTextList[i].text?.y = messageY
+				ui.messageTextList[i].text?.targetX = messageX
+				ui.messageTextList[i].text?.targetY = messageY
 
-			ui.messageTextList[i].text?.draw(shader, dt)
-
-			// Multiple messages
-
-			ui.messageTextList[i].counter += dt
-			messageY -= 0.1f
-
-			// Delete them after x time
-
-			if (ui.messageTextList[i].counter > 2f) {
-				ui.messageTextList.removeAt(i)
-			}
-		}
-		/*
-
-		for (i in 0..<ui.messageList.size) {
-			if (i < ui.messageList.size) {
-
-				// Show a message
-
-				uiText = Text(ui, font, ui.messageList[i], UIRefCorner.TOP_LEFT, messageX, messageY, .8f)
-
-				uiText.draw(shader, dt)
+				ui.messageTextList[i].text?.draw(shader, dt)
 
 				// Multiple messages
 
-				val counter = ui.messageCounter[i] + 1
-				ui.messageCounter[i] = counter
+				ui.messageTextList[i].counter += dt
 				messageY -= 0.1f
 
 				// Delete them after x time
 
-				if (ui.messageCounter[i] > 90) {
-					ui.messageList.removeAt(i)
-					ui.messageCounter.removeAt(i)
+				if (ui.messageTextList[i].counter > 2f) {
+					ui.messageTextList.removeAt(i)
 				}
 			}
-		}
 
-		 */
+		}
 	}
 }
